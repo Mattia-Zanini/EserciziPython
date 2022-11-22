6922af0b6cc65217e5130c0925c4d5b0a311f2f2
Mattia-Zanini <mattzanini2@gmail.com>
2022-11-22 19:56:29 +0100

    completed filterForExcel

--- pandas/removeRowExcel.py
+++ pandas/removeRowExcel.py
@@ -1,15 +1,67 @@
 import pandas as pd
 import numpy as np
+import math as mt
 import string
 import openpyxl
 
 rows = []
 file = []
-count = 0
 fileExist = False
 fileName = None
 column = None
+ris = None
 columnType = int
+
+def CheckNullRow(row):
+    totalLength = len(row)
+    nullCount = 0
+    for i in range(0, totalLength):
+        if "nan" == str(row[i]):
+            nullCount += 1
+    
+    if nullCount == totalLength:
+        return True
+    else:
+        return False
+
+def TransformExcel():
+    count = 0
+    # get the column
+    while True:
+        column = input("Inserisci il carattere della colonna che vuoi usare nel filtro\n")
+        if len(column) == 1:
+            column = ord(column.lower()) - 97
+            # print(column)
+            break
+        
+    #get the filter and the column type
+    _filter = input("Inserisci il filtro\n")
+    try:
+        _filter = int(_filter)
+        columnType = int
+    except:
+        columnType = string
+        
+    print(type(_filter))
+
+    for i in range(0, len(file)):
+        if CheckNullRow(file[i]) == False:
+            find = None
+            if columnType == int:
+                try:
+                    find = int(file[i][column])
+                except:
+                    pass
+            else:
+                find = file[i][column]
+            
+            if find == _filter:
+                rows.append(i)
+                count += 1
+        else:
+            rows.append(i)
+
+    print(f"Trovate {count} righe corrispondenti al tuo filtro")
 
 # read the excel file
 while True:
@@ -21,43 +73,18 @@ while True:
     except KeyboardInterrupt:
         exit(1)
     except:
-        print("il file non esiste\n")    
+        print("il file non esiste\n")
     if fileExist == True:
         break
+    
+TransformExcel()
 
-# get the column
 while True:
-    column = input("Inserisci il carattere della colonna che vuoi usare nel filtro\n")
-    if len(column) == 1:
-        column = ord(column.lower()) - 97
-        # print(column)
-        break
-    
-#get the filter and the column type
-_filter = input("Inserisci il filtro\n")
-try:
-    _filter = int(_filter)
-    columnType = int
-except:
-    columnType = string
-    
-print(type(_filter))
-
-for i in range(0, len(file)):
-    find = None
-    if columnType == int:
-        try:
-            find = int(file[i][column])
-        except:
-            pass
+    ris = input("Vuoi applicare altri filtri? [yes]/[no] ").lower()
+    if ris == "yes":
+        TransformExcel()
     else:
-        find = file[i][column]
-    
-    if find == _filter:
-        rows.append(i)
-        count += 1
-
-print(f"Trovate {count} righe corrispondenti al tuo filtro")
+        break
 
 answ = input("Vuoi salvare il file? [yes]/[no]: ").lower()
 if answ == 'yes':
@@ -66,5 +93,13 @@ if answ == 'yes':
     # delete rows corresponding to the value of each array's element
     df = df.drop(rows)
     # save to xlsx file
-    filepath = 'output.xlsx'
-    df.to_excel(filepath, index=False)
+    df.to_excel('output.xlsx', index=False, header = False)
+    
+    """
+    # Print all first row
+    print(df.loc[0,:])
+    
+    # Convert first row to json
+    ciao = df.loc[0,:].to_json()
+    print(ciao)
+    """
